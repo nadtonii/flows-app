86444418444de38a8f85d1c444da3793a5b863ab
Toni Nađ <me@tnad.xyz>
2025-10-25 01:34:25 +0200

    Implement Paper icon component for third pill

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -3,14 +3,14 @@
  * https://app.paper.design/file/01K8C2AWB4W9CQNM1MJTEDT33R?node=01K8C84F34SXBB7YJMZERS94F9
  * on Oct 25, 2025 at 1:24 AM.
  */
+import PillNavigationIconThree from './icons/PillNavigationIconThree';
+
 export default function Frame() {
   return (
     <div style={{ alignItems: 'center', backgroundColor: '#FFFFFF', borderRadius: 'calc(infinity * 1px)', boxSizing: 'border-box', contain: 'layout', display: 'flex', flexDirection: 'row', gap: '32px', height: 'fit-content', justifyContent: 'center', outline: '1px solid #00000014', overflowWrap: 'break-word', paddingBlock: '16px', paddingInline: '24px', transformOrigin: '0% 0%', width: 'fit-content' }}>
       <div style={{ backgroundImage: 'url("https://workers.paper.design/file-assets/01K8C2AWB4W9CQNM1MJTEDT33R/01K8C8HRCA81Y5HJJ54VC4M3Z6.svg")', backgroundPosition: 'center center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', boxSizing: 'border-box', flexShrink: '0', height: '30px', maxHeight: 'none', maxWidth: 'none', position: 'relative', transformOrigin: '50% 50%', width: '30px' }} />
       <div style={{ backgroundImage: 'url("https://workers.paper.design/file-assets/01K8C2AWB4W9CQNM1MJTEDT33R/01K8C8GC6N349SHHS0KRHFCXS2.svg")', backgroundPosition: 'center center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', boxSizing: 'border-box', flexShrink: '0', height: '30px', maxHeight: 'none', maxWidth: 'none', position: 'relative', transformOrigin: '50% 50%', width: '30px' }} />
-      <div style={{ alignItems: 'center', boxSizing: 'border-box', contain: 'layout', display: 'flex', flexDirection: 'column', flexShrink: '0', height: 'fit-content', justifyContent: 'center', overflowWrap: 'break-word', paddingBlock: 0, paddingInline: 0, transformOrigin: '50% 50%', width: 'fit-content' }}>
-        <div imageUrlTemporary="blob:https://app.paper.design/c06a86c5-af5f-455a-af94-8e99f1978265" style={{ backgroundImage: 'url("blob:https://app.paper.design/c06a86c5-af5f-455a-af94-8e99f1978265")', backgroundPosition: 'center center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', boxSizing: 'border-box', flexShrink: '0', height: '30px', maxHeight: 'none', maxWidth: 'none', position: 'relative', transformOrigin: '50% 50%', width: '30px' }} />
-      </div>
+      <PillNavigationIconThree />
     </div>
   );
 }
---
+++ src/components/icons/PillNavigationIconThree.jsx
@@ -0,0 +1,12 @@
+/**
+ * Code exported from Paper
+ * https://app.paper.design/file/01K8C2AWB4W9CQNM1MJTEDT33R?node=01K8C8AX840SPFQRYVJK6TQN8Q
+ * on Oct 25, 2025 at 1:30 AM.
+ */
+export default function Frame() {
+  return (
+    <div style={{ alignItems: 'center', boxSizing: 'border-box', contain: 'layout', display: 'flex', flexDirection: 'column', flexShrink: '0', height: 'fit-content', justifyContent: 'center', overflowWrap: 'break-word', paddingBlock: 0, paddingInline: 0, transformOrigin: '50% 50%', width: 'fit-content' }}>
+      <div imageUrlTemporary="blob:https://app.paper.design/c06a86c5-af5f-455a-af94-8e99f1978265" style={{ backgroundImage: 'url("blob:https://app.paper.design/c06a86c5-af5f-455a-af94-8e99f1978265")', backgroundPosition: 'center center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', boxSizing: 'border-box', flexShrink: '0', height: '30px', maxHeight: 'none', maxWidth: 'none', position: 'relative', transformOrigin: '50% 50%', width: '30px' }} />
+    </div>
+  );
+}
